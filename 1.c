30cd81a81019675cd9845ab1fe607817c1255d2b
Duda <dudasouza@live.com>
2023-10-16 22:53:43 -0300

    .

--- 1.c
+++ 1.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    printf("teste");
+    
+    int valor,contador, numeros;
+
+    numeros =0;
+
+    while(numeros < 5 ){
+        printf("Digite um Numero: ");
+        scanf("%d", &valor);
+        if(valor < 0){
+            contador ++;
+            numeros++;
+        }
+        else{
+            numeros++;
+        }
+        
+    }
+    printf("%d", contador);
 }
